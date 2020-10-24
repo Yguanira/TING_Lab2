02c285d9203130cb6742ea9a78319964c40680b0
Yguanira <yguanirap@gmail.com>
2020-10-24 19:39:14 +0100

    fix errors and change style

--- routes/servicios.js
+++ routes/servicios.js
@@ -13,7 +13,7 @@ var router = express.Router();
 router.get('/', async (req, res) => {
   try {
     const client = await pool.connect();
-    const result = await client.query('SELECT products_table.id, products_table.name, categories_table.name as category_name, products_table.imageurl, products_table.description FROM products_table INNER JOIN categories_table ON products_table.category_id = categories_table.id');
+    const result = await client.query('SELECT * FROM company_services_table');
     const results = { 'results': (result) ? result.rows : null};
     res.render('servicios', { title: "Servicios", services: results } );
     client.release();
