33bc8ed790ea15b6d2b4b378ffa052bb5727f3e1
Yguanira <yguanirap@gmail.com>
2020-10-24 19:04:00 +0100

    get services from database

--- routes/servicios.js
+++ routes/servicios.js
@@ -1,44 +1,26 @@
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
 var express = require('express');
 var router = express.Router();
 
-// Services
-var services = new Array();
-
-var service1 = new Array();
-service1 ['columsOrder'] = 'order-md-2';
-service1 ['imageUrl'] = 'https://www.beetrack.com/hs-fs/hubfs/Caracter%C3%ADsticas%20del%20servicio%20al%20cliente.jpg?width=1086&name=Caracter%C3%ADsticas%20del%20servicio%20al%20cliente.jpg';
-service1 ['heading'] = 'Heading 1';
-service1 ['subheading'] = 'Subheading 1';
-service1 ['description'] = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-
-services.push(service1)
-
-var service2 = new Array();
-service2 ['columsOrder'] = '';
-service2 ['imageUrl'] = 'https://i1.wp.com/gaztenpresa.org/statics/2016/03/contrato-de-prestacion-de-servicios.jpg?fit=640%2C452&ssl=1';
-service2 ['heading'] = 'Heading 2';
-service2 ['subheading'] = 'Subheading 2';
-service2 ['description'] = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-
-services.push(service2)
-
-var service3 = new Array();
-service3 ['columsOrder'] = 'order-md-2';
-service3 ['imageUrl'] = 'https://www.genwords.com/blog/wp-content/uploads/2017/11/126-scaled.jpg';
-service3 ['heading'] = 'Heading 3';
-service3 ['subheading'] = 'Subheading 3';
-service3 ['description'] = 'Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.';
-
-services.push(service3)
-
-/* ====== GET Services page ====== */
-router.get('/', function(req, res, next) {
-  res.render('servicios', 
-    { 
-      title: 'Servicios',
-      services: services
-    }
-  );
+/* GET db page. */
+router.get('/', async (req, res) => {
+  try {
+    const client = await pool.connect();
+    const result = await client.query('SELECT products_table.id, products_table.name, categories_table.name as category_name, products_table.imageurl, products_table.description FROM products_table INNER JOIN categories_table ON products_table.category_id = categories_table.id');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('servicios', { title: "Servicios", services: results } );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
 });
 
 module.exports = router;
